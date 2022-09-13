8bb52a5a20c8787f373480ab47c498b4b4cf93e4
melrey008 <101406273+melrey008@users.noreply.github.com>
2022-09-13 11:10:18 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltRight)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (input.isGesture(Gesture.TiltLeft)) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
